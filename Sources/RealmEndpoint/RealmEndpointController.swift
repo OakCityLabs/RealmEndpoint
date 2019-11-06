351f6f00bb00ff7d33cd239f6d23843c900ab40f
Jay Lyerly <jay@oakcity.io>
2019-11-06 14:15:07 -0500

    Remove unnecessary RealmEndpointController file

--- Sources/RealmEndpoint/RealmEndpointController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  RealmEndpointController.swift
-//  RealmEndpoint
-//
-//  Created by Jay Lyerly on 11/5/19.
-//  Copyright © 2019 Oak City Labs. All rights reserved.
-//
-
-import Foundation
